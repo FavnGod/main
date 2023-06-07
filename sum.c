4b164673015a1db1841cc016be522868b2c80e48
FavnGod <anime.boy09@mail.ru>
2023-06-07 23:09:37 +0600

    Добавлена возможность вычитания чисел

--- sum.c
+++ sum.c
@@ -3,5 +3,8 @@ int main() {
 int a = 5;
 int b = 3;
 int sum = a + b;
+int difference = a - b;
 printf("Сумма: %d\n", sum);
-return 0; } // VasilevG
+printf("Разность: %d\n", difference);
+return 0;
+} //VasilevG
